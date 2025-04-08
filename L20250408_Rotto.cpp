0746c63502e0d6c59d36094072ced11568b8fb1c
dhxogud <hyong0117@naver.com>
2025-04-08 09:12:51 +0900

    20250408 09:12 실습

--- L20250408_Rotto.cpp
+++ L20250408_Rotto.cpp
@@ -1,5 +1,24 @@
+#include <iostream>
+#define PASSWORD	"645"
+using namespace std;
 
 int main()
 {
+	char InputPassword[10] = { '0', };
+
+	cin >> InputPassword;
+
+	cout << InputPassword << endl;
+
+	for (int i = 0; i < 3; i++)
+	{
+		if (PASSWORD[i] != InputPassword[i])
+		{
+			cout << "´çÃ· ½ÇÆÐ!" << endl;
+			return 0;
+		}
+	}
+	cout << "·Î¶Ç ´çÃ·!" << endl;
+
 	return 0;
 }
